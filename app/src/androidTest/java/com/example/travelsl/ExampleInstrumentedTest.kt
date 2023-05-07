78615ccd560ce37962e51b04c7f1aadbb83197b5
Tharindu De Costa <80523606+tharindudecosta@users.noreply.github.com>
2023-05-07 12:32:27 +0530

    Medical Tourism Testing

--- app/src/androidTest/java/com/example/travelsl/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/travelsl/ExampleInstrumentedTest.kt
@@ -2,11 +2,12 @@ package com.example.travelsl
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import com.example.travelsl.models.DoctorModel
+import com.google.firebase.database.*
 import org.junit.Test
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,4 +22,31 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.travelsl", appContext.packageName)
     }
+
+    private lateinit var database: DatabaseReference
+
+    @Before
+    fun setUp() {
+        database = FirebaseDatabase.getInstance().getReference("Doctors")
+    }
+    @Test
+    fun testInsertUser() {
+
+        val doctorId = database.push().key.toString()
+        val doctorDb = DoctorModel(doctorId,"Bruce Wayne",
+            "dermatology","MediHelp",
+            "012354985", "bw@mail.com", "Western Medicine")
+        database.child("users").child(doctorId).setValue(doctorDb)
+        database.child("users").child(doctorId).addListenerForSingleValueEvent(object :
+            ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val retrievedDoc = snapshot.getValue(doctorDb::class.java)
+                assertEquals(doctorDb, retrievedDoc)
+            }
+            override fun onCancelled(error: DatabaseError) {
+                // handle errors here
+            }
+        })
+    }
+
 }
--- app/src/main/java/com/example/travelsl/activities/MainActivity.kt
+++ app/src/main/java/com/example/travelsl/activities/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.travelsl.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import com.example.travelsl.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnClick: Button
+    private lateinit var message: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun changeText(v: View){
+        btnClick = findViewById(R.id.btnClick)
+        message = findViewById(R.id.tvMessage)
+        message.text = "Hello, User!"
+
+    }
+
 }
