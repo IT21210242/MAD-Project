cc6ba3a392f2d2282a272c228f7de31e33005488
Tharindu De Costa <80523606+tharindudecosta@users.noreply.github.com>
2023-05-14 12:19:31 +0530

    doc list user item activity

--- app/src/main/java/com/example/travelsl/activities/DoctorListItemUserActivity.kt
+++ app/src/main/java/com/example/travelsl/activities/DoctorListItemUserActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.example.travelsl.R
 
 class DoctorListItemUserActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.doctor_activity_list_item_user)
