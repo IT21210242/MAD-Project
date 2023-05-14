796cbdb05415c989aed50e7f110ef9ea06754301
IT21210242 <it21210242@my.sliit.lk>
2023-05-14 10:27:59 +0530

    admin dashboard frontend commit

--- app/src/main/java/com/example/travelsl2/Activities_Chamika/AdminDashboardActivity.kt
+++ app/src/main/java/com/example/travelsl2/Activities_Chamika/AdminDashboardActivity.kt
@@ -1,7 +1,8 @@
-package com.example.travelsl2
+package com.example.travelsl2.Activities_Chamika
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.travelsl2.R
 
 class AdminDashboardActivity : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
